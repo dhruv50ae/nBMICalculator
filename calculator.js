859f72a8e3ac792a7a73043a7ee43134e963d5aa
dhruv50ae <linkdp2012@gmail.com>
2022-05-16 14:35:46 +0100

    Added and created the index file

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
